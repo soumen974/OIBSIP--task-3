a362b9a95a25731d7382757ff8b18cb35e805195
soumen bhunia <sob99338@gmail.com>
2023-10-25 11:49:59 +0530

    js added working good

---
+++ script.js
@@ -0,0 +1,30 @@
+function convertTemperature() {
+    const temperature = parseFloat(document.getElementById("input-temperature").value);
+    const fromUnit = document.getElementById("from-unit").value;
+    const toUnit = document.getElementById("to-unit").value;
+    const resultElement = document.getElementById("result");
+
+    let result;
+    if (fromUnit === toUnit) {
+        result = temperature;
+        resultElement.textContent = `${temperature} ${toUnit} is ${result} ${toUnit}`;
+    } else if (fromUnit === "celsius" && toUnit === "fahrenheit") {
+        result = (temperature * 9/5) + 32;
+        resultElement.textContent = `${temperature} °C is ${result} °F`;
+    } else if (fromUnit === "celsius" && toUnit === "kelvin") {
+        result = temperature + 273.15;
+        resultElement.textContent = `${temperature} °C is ${result} K`;
+    } else if (fromUnit === "fahrenheit" && toUnit === "celsius") {
+        result = (temperature - 32) * 5/9;
+        resultElement.textContent = `${temperature} °F is ${result} °C`;
+    } else if (fromUnit === "fahrenheit" && toUnit === "kelvin") {
+        result = (temperature - 32) * 5/9 + 273.15;
+        resultElement.textContent = `${temperature} °F is ${result} K`;
+    } else if (fromUnit === "kelvin" && toUnit === "celsius") {
+        result = temperature - 273.15;
+        resultElement.textContent = `${temperature} K is ${result} °C`;
+    } else if (fromUnit === "kelvin" && toUnit === "fahrenheit") {
+        result = (temperature - 273.15) * 9/5 + 32;
+        resultElement.textContent = `${temperature} K is ${result} °F`;
+    }
+}
